bf40a2c17b30d6955eaf7d803b83a4452e8c1a4a
raffanfatih <raffan.fatih@gmail.com>
2026-02-27 15:32:10 +0700

    Update praktikum5.latihan4.py

--- Praktikum5/praktikum5.latihan4.py
+++ Praktikum5/praktikum5.latihan4.py
@@ -10,8 +10,7 @@ def kombinasi(n, hasil=""):
     kombinasi(n, hasil + "B") # setelah cabang "A" selesai, fungsi akan menambahkan huruf "B"
 kombinasi(2) # memanggil fungsi untuk mencari semua kombinasi dengan panjang 2 karakter
 
-# bagaimana jumlah kombinasi yang dihasilkan
+# bagaimana jumlah kombinasi yang dihasilkan?
 # Di dalam kode Latihan 4, pada setiap langkah rekursi, fungsi bercabang menjadi 2 pilihan, yaitu menambahkan huruf "A" atau huruf "B".
-# Oleh karena itu, rumus untuk menghitung total kombinasinya adalah:
-# Total Kombinasi=(Jumlah Pilihan)^n
-# Karena jumlah pilihannya ada 2 ("A" dan "B"), maka rumusnya menjadi 2^n
+# Oleh karena itu, rumus untuk menghitung total kombinasinya adalah: Total Kombinasi=(Jumlah Pilihan)^n
+# Karena jumlah pilihannya ada 2 ("A" dan "B"), maka rumusnya menjadi 2^n 
